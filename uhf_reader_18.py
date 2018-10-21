c45ad51554e88c16bfb011729a26950a1a2cadce
loblik <lobl.pavel@gmail.com>
2018-10-21 09:29:58 +0200

    add workmode command

--- uhf_reader_18.py
+++ uhf_reader_18.py
@@ -33,6 +33,31 @@ class UICmdInfo(UICmd):
         ui.printMsg("power     : " + str(msg[3]) + " dBm")
         ui.printMsg("scan time : " + str(int(msg[4])*100) + " ms")
 
+class UICmdWorkMode(UICmd):
+    def __init__(self):
+        super().__init__()
+        self.cmd = "mode"
+        self.descCmd = "gets reader work mode ()"
+
+    def run(self, args, reader, ui):
+         msg = reader.getWorkMode()
+         ui.printMsg("mode              : " + msg[1])
+         ui.printMsg("protocol          : " + msg[2])
+         ui.printMsg("buzzer            : " + msg[3])
+         ui.printMsg("addr mode         : " + msg[4])
+         ui.printMsg("interface         : " + msg[5])
+         ui.printMsg("storage           : " + msg[6])
+         ui.printMsg("offset            : " + hex(msg[7]))
+         ui.printMsg("length            : " + str(msg[8]))
+         ui.printMsg("filter time       : " + str(msg[9]) + " s")
+         ui.printMsg("accuracy EAS      : " + str(msg[10]))
+         ui.printMsg("offset time       : " + str(msg[11]) + " ms")
+         ui.printMsg("W byte order      : " + msg[12])
+         ui.printMsg("W mode            : " + msg[13])
+         ui.printMsg("W output interval : " + str(msg[14] * 10) + " ms")
+         ui.printMsg("W pulse width     : " + str(msg[15] * 10) + " us")
+         ui.printMsg("W pulse period    : " + str(msg[16] * 100) + " us")
+
 class UICmdSetFreq(UICmd):
     def __init__(self):
         super().__init__()
@@ -113,6 +138,7 @@ class UI:
         self.addCommand(UICmdQuit())
         self.addCommand(UICmdSetFreq())
         self.addCommand(UICmdSetScanTime())
+        self.addCommand(UICmdWorkMode())
         self.reader = reader
 #        readline.parse_and_bind('tab: complete')
 #        readline.parse_and_bind('set editing-mode vi')
@@ -156,11 +182,46 @@ class UI:
         
 
 class UHFReader18:
+    PROTO_6B = "ISO18000-6B"
+    PROTO_6C = "ISO18000-6C"
+
+    IFACE_WIEGAND = "wiegand"
+    IFACE_SERIAL = "RS232/RS485"
+    IFACE_SYRIS = "SYRIS485"
+
+    MSB = "msb"
+    LSB = "lsb"
+
+    WIEGAND_26 = "wiegand26"
+    WIEGAND_34 = "wiegand34"
+
+    STORAGE_PASSWD = "password"
+    STORAGE_EPC = "epc"
+    STORAGE_TID = "tid"
+    STORAGE_USER = "user"
+    STORAGE_MULTI_QUERY = "multi query"
+    STORAGE_ONE_QUERY = "one query"
+    STORAGE_EAS = "eas"
+
+    MODE_ANSWER = "answer"
+    MODE_ACTIVE= "active"
+    MODE_TRIG_LOW = "trigger low"
+    MODE_TRIG_HIGH = "trigger high"
+
+    ON = "on"
+    OFF = "off"
+
+    ADDR_BYTE = "byte"
+    ADDR_WORD = "word"
+
     GET_READER_INFO = 0x21
     SET_FREQ = 0x22
     SET_SCAN_TIME = 0x25
     SET_POWER = 0x2f
+    GET_WORK_MODE = 0x36
     ADDR_BROADCAST = 0xff
+
+
     def openPort(self, path, baud):
         self.port = path
         self.sr = serial.Serial()
@@ -210,7 +271,35 @@ class UHFReader18:
         self.send(self.ADDR_BROADCAST, self.SET_POWER, bytes([power]))
         return self.retAddr(self.recv())
 
+    def getWorkMode(self, addr = 0xff):
+        self.send(addr, self.GET_WORK_MODE)
+        recv = self.recv()
+        mode = [ self.MODE_ANSWER, self.MODE_ACTIVE, self.MODE_TRIG_LOW, self.MODE_TRIG_HIGH ][recv[7]]
+        chaos = recv[8]
+        proto = [ self.PROTO_6C, self.PROTO_6B ][chaos & 0x1]
+        buzzer = [ self.ON, self.OFF ][(chaos & 0x4) >> 2]
+        addr = [ self.ADDR_WORD, self.ADDR_BYTE][(chaos & 0x8) >> 3]
+        ifce = [ self.IFACE_WIEGAND, self.IFACE_SERIAL, self.IFACE_SYRIS ][((chaos & 0x10) >> 4) + ((chaos & 0x2) >> 1)]
+        storage = [ self.STORAGE_PASSWD, self.STORAGE_EPC, self.STORAGE_TID, self.STORAGE_USER, self.STORAGE_MULTI_QUERY, self.STORAGE_ONE_QUERY, self.STORAGE_EAS ][recv[9]]
+        offset = recv[10]
+        length = recv[11]
+        filter_time = recv[12]
+        acc_eas = recv[13]
+        offset_time = recv[14]
+        w_order = [ self.MSB, self.LSB ][(recv[3] & 0x2) >> 1]
+        w_type = [ self.WIEGAND_26, self.WIEGAND_34 ][recv[3] & 0x1]
+        w_inter = recv[4]
+        w_pulse_width = recv[5]
+        w_pulse_int = recv[6]
+        return (recv[0], mode, proto, buzzer, addr, ifce, storage, offset, length, filter_time, acc_eas, offset_time, w_order, w_type, w_inter, w_pulse_width, w_pulse_int)
+
+
+if len(sys.argv) != 2:
+    print("usage: uhf TTY_SERIAL")
+    sys.exit(2)
+	
+
 uhfr = UHFReader18()
-uhfr.openPort("/dev/ttyUSB1", 57600)
+uhfr.openPort(sys.argv[1], 57600)
 
 UI(uhfr).run()
