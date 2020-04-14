7742402f958352be03ca5c3dfcbbb0288d192072
sonken625 <1113146c@gmail.com>
2020-04-14 19:12:17 +0900

    make methods  camelCase and static method if it should be.

--- uhf_reader_18.py
+++ uhf_reader_18.py
@@ -13,88 +13,97 @@ class UICmd:
         self.cmd = cmd
         self.desc_cmd = desc_cmd
 
-    def printOk(self, addr, ui):
-        ui.printMsg("reader " + hex(addr) + ": OK")
+    @staticmethod
+    def print_ok(addr, ui):
+        ui.print_msg("reader " + hex(addr) + ": OK")
 
     def validate(self, args, ui):
         return True
 
-    def getUsage(self):
-        return (self.cmd + " " + self.descArgs).ljust(16) + self.descCmd
+    def get_usage(self):
+        return (self.cmd + " " + self.desc_args).ljust(16) + self.desc_cmd
+
 
 class UICmdInfo(UICmd):
     def __init__(self):
         super().__init__("info", "gets reader settings (freq, power, scantime)")
 
     def run(self, args, reader, ui):
-        msg = reader.getReaderInfo()
-        ui.printMsg("address   : " + hex(msg[0]))
-        ui.printMsg("low freq  : " + str(msg[1]) + " kHz")
-        ui.printMsg("high freq : " + str(msg[2]) + " kHz")
-        ui.printMsg("power     : " + str(msg[3]) + " dBm")
-        ui.printMsg("scan time : " + str(int(msg[4])*100) + " ms")
+        msg = reader.get_reader_info()
+        ui.print_msg("address   : " + hex(msg[0]))
+        ui.print_msg("low freq  : " + str(msg[1]) + " kHz")
+        ui.print_msg("high freq : " + str(msg[2]) + " kHz")
+        ui.print_msg("power     : " + str(msg[3]) + " dBm")
+        ui.print_msg("scan time : " + str(int(msg[4]) * 100) + " ms")
+
 
 class UICmdWorkMode(UICmd):
     def __init__(self):
         super().__init__("mode", "gets reader work mode ()")
 
     def run(self, args, reader, ui):
-         msg = reader.getWorkMode()
-         ui.printMsg("mode              : " + msg[1])
-         ui.printMsg("protocol          : " + msg[2])
-         ui.printMsg("buzzer            : " + msg[3])
-         ui.printMsg("addr mode         : " + msg[4])
-         ui.printMsg("interface         : " + msg[5])
-         ui.printMsg("storage           : " + msg[6])
-         ui.printMsg("offset            : " + hex(msg[7]))
-         ui.printMsg("length            : " + str(msg[8]))
-         ui.printMsg("filter time       : " + str(msg[9]) + " s")
-         ui.printMsg("accuracy EAS      : " + str(msg[10]))
-         ui.printMsg("offset time       : " + str(msg[11]) + " ms")
-         ui.printMsg("W byte order      : " + msg[12])
-         ui.printMsg("W mode            : " + msg[13])
-         ui.printMsg("W output interval : " + str(msg[14] * 10) + " ms")
-         ui.printMsg("W pulse width     : " + str(msg[15] * 10) + " us")
-         ui.printMsg("W pulse period    : " + str(msg[16] * 100) + " us")
+        msg = reader.get_work_mode()
+        ui.print_msg("mode              : " + msg[1])
+        ui.print_msg("protocol          : " + msg[2])
+        ui.print_msg("buzzer            : " + msg[3])
+        ui.print_msg("addr mode         : " + msg[4])
+        ui.print_msg("interface         : " + msg[5])
+        ui.print_msg("storage           : " + msg[6])
+        ui.print_msg("offset            : " + hex(msg[7]))
+        ui.print_msg("length            : " + str(msg[8]))
+        ui.print_msg("filter time       : " + str(msg[9]) + " s")
+        ui.print_msg("accuracy EAS      : " + str(msg[10]))
+        ui.print_msg("offset time       : " + str(msg[11]) + " ms")
+        ui.print_msg("W byte order      : " + msg[12])
+        ui.print_msg("W mode            : " + msg[13])
+        ui.print_msg("W output interval : " + str(msg[14] * 10) + " ms")
+        ui.print_msg("W pulse width     : " + str(msg[15] * 10) + " us")
+        ui.print_msg("W pulse period    : " + str(msg[16] * 100) + " us")
+
 
 class UICmdSetFreq(UICmd):
     def __init__(self):
         super().__init__("freq", "set frequency range", "LOW HIGH", 2)
 
-    def isFreq(self, f):
+    @staticmethod
+    def is_freq(f):
         if f.isdigit():
-            if int(f) >= 902600 and int(f) <= 927400:
+            if 902600 <= int(f) <= 927400:
                 if (int(f) - 902600) % 400 == 0:
                     return True
 
-    def toFreq(self, f):
+    @staticmethod
+    def to_freq(f):
         return int((int(f) - 902600) / 400)
 
     def validate(self, args, ui):
-        return self.isFreq(args[0]) and self.isFreq(args[1]) and int(args[0]) <= int(args[1])
+        return self.is_freq(args[0]) and self.is_freq(args[1]) and int(args[0]) <= int(args[1])
 
     def run(self, args, reader, ui):
-        self.printOk(reader.setFreq(self.toFreq(args[0]), self.toFreq(args[1])), ui)
+        self.print_ok(reader.set_freq(self.to_freq(args[0]), self.to_freq(args[1])), ui)
+
 
 class UICmdSetPower(UICmd):
     def __init__(self):
         super().__init__("power", "sets power in dBm", "0-30", 1)
 
     def validate(self, args, ui):
-        return args[0].isdigit() and (int(args[0]) >= 0 and int(args[0]) <= 30)
+        return args[0].isdigit() and (0 <= int(args[0]) <= 30)
 
     def run(self, args, reader, ui):
-        self.printOk(reader.setPower(int(args[0])), ui)
+        self.print_ok(reader.set_power(int(args[0])), ui)
+
 
 class UICmdSetScanTime(UICmd):
     def __init__(self):
         super().__init__("scantime", "sets scan time in 100's of ms", "3-255", 1)
 
     def validate(self, args, ui):
-        return args[0].isdigit() and (int(args[0]) >= 2 and int(args[0]) <= 255)
+        return args[0].isdigit() and (2 <= int(args[0]) <= 255)
 
     def run(self, args, reader, ui):
-        self.printOk(reader.setScanTime(int(args[0])), ui)
+        self.print_ok(reader.set_scan_time(int(args[0])), ui)
+
 
 class UICmdQuit(UICmd):
     def __init__(self):
@@ -109,24 +118,33 @@ class UICmdHelp(UICmd):
         super().__init__("help", "print commands and usage")
 
     def run(self, args, reader, ui):
-        ui.puts(ui.getHelp())
- 
+        ui.puts(ui.get_help())
+
 
 class UI:
     def __init__(self, reader):
         self.commands = dict()
-        self.addCommand(UICmdInfo())
-        self.addCommand(UICmdSetPower())
-        self.addCommand(UICmdHelp())
-        self.addCommand(UICmdQuit())
-        self.addCommand(UICmdSetFreq())
-        self.addCommand(UICmdSetScanTime())
-        self.addCommand(UICmdWorkMode())
+        self.add_command(UICmdInfo())
+        self.add_command(UICmdSetPower())
+        self.add_command(UICmdHelp())
+        self.add_command(UICmdQuit())
+        self.add_command(UICmdSetFreq())
+        self.add_command(UICmdSetScanTime())
+        self.add_command(UICmdWorkMode())
         self.reader = reader
-#        readline.parse_and_bind('tab: complete')
-#        readline.parse_and_bind('set editing-mode vi')
 
-    def addCommand(self, cmd):
+    #        readline.parse_and_bind('tab: complete')
+    #        readline.parse_and_bind('set editing-mode vi')
+
+    @staticmethod
+    def puts(text):
+        print(text, end='')
+
+    @staticmethod
+    def print_msg(msg):
+        print(msg)
+
+    def add_command(self, cmd):
         self.commands[cmd.cmd] = cmd
 
     def input(self, line):
@@ -136,23 +154,17 @@ class UI:
             if len(args[1:]) == cmd.args and cmd.validate(args[1:], self):
                 cmd.run(args[1:], self.reader, self)
             else:
-                self.printMsg(cmd.getUsage())
+                self.print_msg(cmd.get_usage())
 
-    def getHelp(self):
+    def get_help(self):
         usage = ""
         for k, v in sorted(self.commands.items()):
-            usage = usage + v.getUsage() + "\n"
+            usage = usage + v.get_usage() + "\n"
         return usage
 
     def quit(self):
-        self.printMsg("bye")
+        self.print_msg("bye")
         sys.exit(0)
-
-    def puts(self, text):
-        print(text, end='')
-
-    def printMsg(self, msg):
-        print(msg)
 
     def run(self):
         while True:
@@ -161,8 +173,8 @@ class UI:
             except EOFError:
                 self.quit()
             except KeyboardInterrupt:
-                self.printMsg('')
-        
+                self.print_msg('')
+
 
 class UHFReader18:
     PROTO_6B = "ISO18000-6B"
@@ -187,7 +199,7 @@ class UHFReader18:
     STORAGE_EAS = "eas"
 
     MODE_ANSWER = "answer"
-    MODE_ACTIVE= "active"
+    MODE_ACTIVE = "active"
     MODE_TRIG_LOW = "trigger low"
     MODE_TRIG_HIGH = "trigger high"
 
@@ -204,16 +216,19 @@ class UHFReader18:
     GET_WORK_MODE = 0x36
     ADDR_BROADCAST = 0xff
 
+    @staticmethod
+    def ret_addr(data):
+        return data[0]
 
-    def openPort(self, path, baud):
+    def open_port(self, path, baud):
         self.port = path
         self.sr = serial.Serial()
-        self.sr.baudrate = baud 
+        self.sr.baudrate = baud
         self.sr.port = path
         self.sr.open()
         self.crc = crcmod.predefined.Crc('crc-16-mcrf4xx')
 
-    def getCrc(self, msg):
+    def get_crc(self, msg):
         crc = self.crc.new()
         crc.update(msg)
         return crc.digest()
@@ -221,7 +236,7 @@ class UHFReader18:
     def send(self, addr, cmd, args=bytes([])):
         msg = bytes([addr, cmd]) + args
         msg = bytes([len(msg) + 2]) + msg
-        crc = self.getCrc(msg)
+        crc = self.get_crc(msg)
         msg = msg + bytes([crc[1], crc[0]])
         self.sr.write(msg)
 
@@ -229,32 +244,29 @@ class UHFReader18:
         count = self.sr.read(1)
         data = self.sr.read(count[0])
         crc = data[-2:]
-        calcCrc = self.getCrc(count + data[:-2])
+        calcCrc = self.get_crc(count + data[:-2])
         if crc[0] != calcCrc[1] or crc[1] != calcCrc[0]:
             raise Exception("CRC failed: " + (count + data).hex())
         return data
 
-    def getReaderInfo(self):
+    def get_reader_info(self):
         self.send(self.ADDR_BROADCAST, self.GET_READER_INFO)
         info = self.recv()
-        return (info[0], info[8] * 400 + 902600, info[7] * 400 + 902600, info[9], info[10])
+        return info[0], info[8] * 400 + 902600, info[7] * 400 + 902600, info[9], info[10]
 
-    def retAddr(self, data):
-        return data[0]
-
-    def setFreq(self, low, high):
+    def set_freq(self, low, high):
         self.send(self.ADDR_BROADCAST, self.SET_FREQ, bytes([high, low]))
-        return self.retAddr(self.recv())
+        return self.ret_addr(self.recv())
 
-    def setScanTime(self, time):
+    def set_scan_time(self, time):
         self.send(self.ADDR_BROADCAST, self.SET_SCAN_TIME, bytes([time]))
-        return self.retAddr(self.recv())
+        return self.ret_addr(self.recv())
 
-    def setPower(self, power):
+    def set_power(self, power):
         self.send(self.ADDR_BROADCAST, self.SET_POWER, bytes([power]))
-        return self.retAddr(self.recv())
+        return self.ret_addr(self.recv())
 
-    def getWorkMode(self, addr = 0xff):
+    def get_work_mode(self, addr=0xff):
         self.send(addr, self.GET_WORK_MODE)
         recv = self.recv()
         mode = [ self.MODE_ANSWER, self.MODE_ACTIVE, self.MODE_TRIG_LOW, self.MODE_TRIG_HIGH ][recv[7]]
@@ -280,9 +292,8 @@ class UHFReader18:
 if len(sys.argv) != 2:
     print("usage: uhf TTY_SERIAL")
     sys.exit(2)
-	
 
 uhfr = UHFReader18()
-uhfr.openPort(sys.argv[1], 57600)
+uhfr.open_port(sys.argv[1], 57600)
 
 UI(uhfr).run()
